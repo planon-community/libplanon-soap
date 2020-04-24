cac6a8a8989279d576de96cb9f971db6a386f71f
Douglas Plumley <douglas.plumley@dartmouth.edu>
2020-04-24 11:10:40 -0400

    Moved code from planon_api.py to __init__, the extra file was unneccessary at this time.

--- libplanon/__init__.py
+++ libplanon/__init__.py
@@ -1,1 +1,60 @@
-from . import planon_api
+import time
+import os
+import json
+import logging
+
+import zeep
+import zeep.cache
+import requests
+
+# ************************************************************************************************
+# SETUP 
+# ************************************************************************************************
+
+log = logging.getLogger(__name__)
+
+session = requests.Session()
+transport = zeep.Transport(cache = zeep.cache.InMemoryCache(), session = session)
+
+# *************************************************************************************************
+# CLASSES
+# *************************************************************************************************
+
+class TokenManager():
+    """Manages SOAP API tokens for Planon
+
+    """
+
+    token_default_expires = 28800
+
+    def __init__(self, url, username, password, token_age_threshold=900):
+        self.url = url
+        self.username = username
+        self._password = password
+        self.token_age_threshold = token_age_threshold
+
+        self.token_wrapper = {
+            'expires': None,
+            'token': None
+        }
+
+        self.session_client = zeep.Client(wsdl=f'{self.url}/PlanonSession?wsdl', transport=transport)
+
+    def get_token(self):
+        if self.token_wrapper['expires'] and self.token_wrapper['expires'] - time.time() > self.token_age_threshold:
+            log.debug('Existing token is within age threshold, returning.')
+            return self.token_wrapper['token']
+        else:
+            log.info('Requesting new token.')
+
+            try:
+                token = self.session_client.service.login(self.username, self._password)
+            except zeep.exceptions.Fault as e:
+                if e.message == 'unknown':
+                    log.error('Planon session SOAP API "unknown" errors are typically due to invalid passwords')
+                    raise e
+
+            self.token_wrapper['token'] = token
+            self.token_wrapper['expires'] = time.time() + self.token_default_expires
+
+            return self.token_wrapper['token']
